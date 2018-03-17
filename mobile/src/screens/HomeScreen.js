89b172568fc37b792c7b01bf45999a04d205a577
RANDRIANANDRASANA Pascal Elysé <herypaslie.dell@gmail.com>
2018-03-17 14:47:13 +0300

    Dynamics data

--- mobile/src/screens/HomeScreen.js
+++ mobile/src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import { ActivityIndicator, FlatList } from 'react-native';
 
 import styled from '../utils/styled';
 
 import FeedCard from '../components/FeedCard/FeedCard';
+import GET_TWEETS_QUERY from '../graphql/queries/getTweets';
 
 const Root = styled.View`
   background-color: #f2f2f2;
@@ -27,28 +30,36 @@ class HomeScreen extends Component {
     // this.onFavoritePress = this.favorite.bind(this);
   }
 
+  _renderItem = ({ item }) => <FeedCard {...item} />
+
   onFavoritePress() {
     console.log(this.state);
   }
 
   render() {
+    const { data } = this.props;
+    console.log('data', data);
+    if (data.loading) {
+      return (
+        <Root>
+          <ActivityIndicator size="large" />
+        </Root>
+      )
+    }
+
     return (
       <Root>
         <ScrollView>
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
-          <FeedCard onFavoritePress={this.onFavoritePress} />
+          <FlatList
+            contentContainerStyle={{ alignSelf: 'stretch' }}
+            data={data.getTweets}
+            keyExtractor={item => item._id}
+            renderItem={this._renderItem}
+          />
         </ScrollView>
       </Root>
     );
   }
 }
 
-export default HomeScreen;
+export default graphql(GET_TWEETS_QUERY)(HomeScreen);
