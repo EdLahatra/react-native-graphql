4c38398c5eff1b15536a19102d746ea49049c5ad
RANDRIANANDRASANA Pascal Elysé <herypaslie.dell@gmail.com>
2018-03-17 13:37:11 +0300

    Create navigation react-navigation": "1.0.0-beta.11",
    react-navigation": "1.0.0-beta.11",

---
+++ mobile/src/navigations.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { StackNavigator, TabNavigator, addNavigationHelpers } from 'react-navigation';
+import { BackHandler } from 'react-native';
+import { connect } from 'react-redux';
+import styled from './utils/styled';
+
+import HomeScreen from './screens/HomeScreen';
+
+import ExploreScreen from './screens/ExploreScreen';
+import NotificationsScreen from './screens/NotificationsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+import { colors } from './utils/constants';
+
+const T = styled.Text``;
+
+const Tabs = TabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: () => ({
+        headerTitle: 'Home',
+        tabBarIcon: ({ tintColor }) => <T>Home</T>,
+      }),
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: () => ({
+        headerTitle: 'Explore',
+        tabBarIcon: ({ tintColor }) => <T>Explore</T>,
+      }),
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: () => ({
+        headerTitle: 'Notifications',
+        tabBarIcon: ({ tintColor }) => <T>Notifications</T>,
+      }),
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: () => ({
+        headerTitle: 'Profile',
+        tabBarIcon: ({ tintColor }) => <T>Profile</T>,
+      }),
+    },
+  },
+  {
+    lazy: true,
+    tabBarPosition: 'bottom',
+    swipeEnabled: false,
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: false,
+      activeTintColor: colors.PRIMARY,
+      inactiveTintColor: colors.LIGHT_GRAY,
+      style: {
+        backgroundColor: colors.WHITE,
+        height: 50,
+        paddingVertical: 5,
+      },
+    },
+  },
+);
+
+
+const AppMainNav = StackNavigator(
+  {
+    Home: {
+      screen: Tabs,
+    },
+  },
+  {
+    cardStyle: {
+      backgroundColor: '#F1F6FA',
+    },
+    navigationOptions: () => ({
+      headerStyle: {
+        backgroundColor: colors.WHITE,
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        color: colors.SECONDARY,
+      },
+    }),
+  },
+);
+
+class AppNavigator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterSearch: 'notApproved',
+    };
+
+    this.onBackPress = this.onBackPress.bind(this);
+  }
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => this.onBackPress());
+  }
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', () => this.onBackPress());
+  }
+  onBackPress() {
+    // eslint-disable-next-line
+    const { dispatch, nav } = this.props;
+    console.log('testts');
+    if (nav.index === 0) {
+      return false;
+    }
+    // dispatch(NavigationActions.back());
+    return true;
+  }
+
+  render() {
+    // eslint-disable-next-line
+    const { dispatch, nav, isAuthenticated } = this.props;
+    console.log('dispatch', nav, isAuthenticated);
+    const navig = addNavigationHelpers({
+      dispatch,
+      state: nav,
+    });
+
+    return <AppMainNav navigation={navig} />;
+  }
+}
+
+export default connect(state => ({
+  nav: state.nav,
+  isAuthenticated: state.user.isAuthenticated,
+}))(AppNavigator);
+
+export const router = AppMainNav.router;
