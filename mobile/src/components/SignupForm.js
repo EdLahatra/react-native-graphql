e56a801a5006291010f0aafcf0fce46e00fca847
RANDRIANANDRASANA Pascal Elysé <herypaslie.dell@gmail.com>
2018-03-17 15:31:38 +0300

    Create SignupForm

---
+++ mobile/src/components/SignupForm.js
@@ -0,0 +1,182 @@
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+import Touchable from '@appandflow/touchable';
+import { Platform, Keyboard, AsyncStorage } from 'react-native';
+import { graphql, compose } from 'react-apollo';
+import { connect } from 'react-redux';
+
+import { colors, fakeAvatar } from '../utils/constants';
+import SIGNUP_MUTATION from '../graphql/mutations/signup';
+import Loading from '../components/Loading';
+import { login } from '../actions/user';
+
+const Root = styled(Touchable).attrs({
+  feedback: 'none',
+})`
+  flex: 1;
+  position: relative;
+  justifyContent: center;
+  alignItems: center;
+`;
+
+const Wrapper = styled.View`
+  alignSelf: stretch;
+  alignItems: center;
+  justifyContent: center;
+  flex: 1;
+`;
+
+const BackButton = styled(Touchable).attrs({
+  feedback: 'opacity',
+  hitSlop: { top: 20, bottom: 20, right: 20, left: 20 },
+})`
+  justifyContent: center;
+  alignItems: center;
+  position: absolute;
+  top: 5%;
+  zIndex: 1;
+  left: 5%;
+`;
+
+const ButtonConfirm = styled(Touchable).attrs({
+  feedback: 'opacity',
+})`
+  position: absolute;
+  bottom: 15%;
+  width: 70%;
+  height: 50;
+  background-color: ${props => props.theme.PRIMARY};
+  border-radius: 10;
+  justify-content: center;
+  align-items: center;
+  shadowOpacity: 0.2;
+  shadowRadius: 5;
+  shadowOffset: 0px 2px;
+  elevation: 2;
+`;
+
+const ButtonConfirmText = styled.Text`
+  color: ${props => props.theme.WHITE};
+  font-weight: 600;
+`;
+
+const InputWrapper = styled.View`
+  height: 50;
+  width: 70%;
+  border-bottom-width: 2;
+  border-bottom-color: ${props => props.theme.LIGHT_GRAY};
+  justifyContent: flex-end;
+`;
+
+const T = styled.Text``;
+
+const Input = styled.TextInput.attrs({
+  placeholderTextColor: colors.LIGHT_GRAY,
+  selectionColor: Platform.OS === 'ios' ? colors.PRIMARY : undefined,
+  autoCorrect: false,
+})`
+  height: 30;
+  color: ${props => props.theme.WHITE};
+`;
+
+class SignupForm extends Component {
+  state = {
+    fullName: '',
+    email: '',
+    password: '',
+    username: '',
+    loading: false,
+  };
+
+  _onOutsidePress = () => Keyboard.dismiss();
+
+  _onChangeText = (text, type) => this.setState({ [type]: text });
+
+  _checkIfDisabled() {
+    const { fullName, email, password, username } = this.state;
+
+    if (!fullName || !email || !password || !username) {
+      return true;
+    }
+
+    return false;
+  }
+
+  _onSignupPress = async () => {
+    this.setState({ loading: true });
+
+    const { fullName, email, password, username } = this.state;
+    const avatar = fakeAvatar;
+
+    try {
+      const { data } = await this.props.mutate({
+        variables: {
+          fullName,
+          email,
+          password,
+          username,
+          avatar,
+        },
+      });
+      await AsyncStorage.setItem('@twitteryoutubeclone', data.signup.token);
+      this.setState({ loading: false });
+      return this.props.login();
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  render() {
+    if (this.state.loading) {
+      return <Loading />;
+    }
+    return (
+      <Root onPress={this._onOutsidePress}>
+        <BackButton onPress={this.props.onBackPress}>
+          <T>onBackPress</T>
+        </BackButton>
+        <Wrapper>
+          <InputWrapper>
+            <Input
+              placeholder="Full Name"
+              autoCapitalize="words"
+              onChangeText={text => this._onChangeText(text, 'fullName')}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              placeholder="Email"
+              autoCapitalize="none"
+              keyboardType="email-address"
+              onChangeText={text => this._onChangeText(text, 'email')}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              placeholder="Password"
+              secureTextEntry
+              onChangeText={text => this._onChangeText(text, 'password')}
+            />
+          </InputWrapper>
+          <InputWrapper>
+            <Input
+              placeholder="Username"
+              autoCapitalize="none"
+              onChangeText={text => this._onChangeText(text, 'username')}
+            />
+          </InputWrapper>
+        </Wrapper>
+        <ButtonConfirm
+          onPress={this._onSignupPress}
+          disabled={this._checkIfDisabled()}
+        >
+          <ButtonConfirmText>Sign Up</ButtonConfirmText>
+        </ButtonConfirm>
+      </Root>
+    );
+  }
+}
+
+export default compose(graphql(SIGNUP_MUTATION), connect(undefined, { login }))(
+  SignupForm,
+);
