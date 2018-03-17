27e935ec87d1761b2be65f3e4634e0d96c9b6808
RANDRIANANDRASANA Pascal Elysé <herypaslie.dell@gmail.com>
2018-03-17 16:15:42 +0300

    Add method _checkIfToken

--- mobile/App.js
+++ mobile/App.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { UIManager } from 'react-native';
+import { UIManager, AsyncStorage } from 'react-native';
 import { ApolloProvider } from 'react-apollo';
 import { ThemeProvider } from 'styled-components';
 
 import { store, client } from './src/store';
 import { colors } from './src/utils/constants';
 
+import { login } from './src/actions/user';
+
 import AppNavigation from './src/navigations';
+import Loading from './src/components/Loading';
 
 if (UIManager.setLayoutAnimationEnabledExperimental) {
   UIManager.setLayoutAnimationEnabledExperimental(true);
 }
 
 export default class App extends React.Component {
+  state = {
+    appIsReady: false,
+  }
+
+  componentWillMount() {
+    this._checkIfToken();
+  }
+
+  _checkIfToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('@twitteryoutubeclone');
+      if (token != null) {
+        store.dispatch(login());
+      }
+    } catch (error) {
+      throw error;
+    }
+
+    this.setState({ appIsReady: true });
+  }
+
   render() {
+    if (!this.state.appIsReady) {
+      return <Loading />
+    }
     return (
       <ApolloProvider store={store} client={client}>
         <ThemeProvider theme={colors}>
