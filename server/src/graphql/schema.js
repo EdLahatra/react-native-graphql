f5f8ad963fb6d2e766d5136b53b0a4371b55d8f9
RANDRIANANDRASANA Pascal Elysé <herypaslie.dell@gmail.com>
2018-03-17 20:40:45 +0300

    Add subscriptions on graphQL in Schema

--- server/src/graphql/schema.js
+++ server/src/graphql/schema.js
@@ -1,14 +1,11 @@
 export default `
   scalar Date
-
   type Status {
     message: String!
   }
-
   type Auth {
     token: String!
   }
-
   type User {
     _id: ID!
     username: String
@@ -19,7 +16,6 @@ export default `
     createdAt: Date!
     updatedAt: Date!
   }
-
   type Me {
     _id: ID!
     username: String
@@ -30,7 +26,6 @@ export default `
     createdAt: Date!
     updatedAt: Date!
   }
-
   type Tweet {
     _id: ID!
     text: String!
@@ -39,14 +34,12 @@ export default `
     createdAt: Date!
     updatedAt: Date!
   }
-
   type Query {
     getTweet(_id: ID!): Tweet
     getTweets: [Tweet]
     getUserTweets: [Tweet]
     me: Me
   }
-
   type Mutation {
     createTweet(text: String!): Tweet
     updateTweet(_id: ID!, text: String): Tweet
@@ -54,9 +47,12 @@ export default `
     signup(email: String!, fullName: String!, password: String!, avatar: String, username: String): Auth
     login(email: String!, password: String!): Auth
   }
-
+  type Subscription {
+    tweetAdded: Tweet
+  }
   schema {
     query: Query
     mutation: Mutation
+    subscription: Subscription
   }
 `;
